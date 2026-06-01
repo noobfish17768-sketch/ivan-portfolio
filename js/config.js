504f3171f4727cc3a09e19f232609e73df57a0e2
Ivan <noobfish17768@gmail.com>
2026-06-01 16:30:20 +0700

    delete .gitignore

---
+++ js/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+
+    email: "ivanadrian17768@gmail.com",
+
+    whatsapp: "6281882882224",
+
+    linkedin:
+        "https://www.linkedin.com/in/ivan-adrian-b254a2259/",
+
+    github:
+        "https://github.com/noobfish17768-sketch"
+
+};
